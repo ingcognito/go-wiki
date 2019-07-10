1064df3dbe872b84009f2dc23befbd41be755c8c
noah-ing <ing.noah@gmail.com>
2019-07-10 11:44:19 -0400

    Fixed database query to handle Uppercase titles

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"regexp"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"github.com/nlopes/slack"
 )
 
 type wikiPage struct {
@@ -88,6 +91,44 @@ func main() {
 	// Graceful Shutdown
 	waitForShutdown(srv)
 
+	//Slack
+	token := "xoxp-635227745970-647965568001-675714514899-6faeca738b8fefb3ea4a21f4953054c7"
+	api := slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+
+			case *slack.MessageEvent:
+				info := rtm.GetInfo()
+
+				text := ev.Text
+				text = strings.TrimSpace(text)
+				text = strings.ToLower(text)
+
+				matched, _ := regexp.MatchString("dark souls", text)
+
+				if ev.User != info.User.ID && matched {
+					rtm.SendMessage(rtm.NewOutgoingMessage("\\[T]/ Praise the Sun \\[T]/", ev.Channel))
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+				// Take no action
+			}
+		}
+	}
 }
 
 func waitForShutdown(srv *http.Server) {
@@ -112,32 +153,29 @@ func getWiki(w http.ResponseWriter, r *http.Request) {
 	var wikiExtract string
 
 	query := r.URL.Query()
-	wikiTitle = query.Get("title")
+	wikiTitle = strings.Title(query.Get("title"))
 	if wikiTitle == "" {
 		w.Write([]byte(fmt.Sprintf("ENTER A TITLE")))
 		return
 	}
 
 	db, err := sql.Open("postgres", "user=postgres dbname=bot host=db port=5432 sslmode=disable")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 	fmt.Println(wikiTitle)
 
-	sqlStatement := `SELECT title FROM pages where title=$1;`
-	rows, err := db.Query(sqlStatement, wikiTitle)
-	var rowCount int = 0
-	for rows.Next() {
-		rowCount++
-		err := rows.Scan(&wikiTitle, &wikiExtract)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("this is coming from the database")
+	sqlStatement := `SELECT title, extract FROM pages where title=$1;`
+	err = db.QueryRow(sqlStatement, wikiTitle).Scan(&wikiTitle, &wikiExtract)
+	fmt.Println(wikiExtract)
+	fmt.Printf("this is coming from the database")
+	if err != nil {
+		log.Println(err)
 	}
-	if rowCount == 0 {
+
+	//If database does not contain wiki page then store it
+	if wikiExtract == "" {
 		resp, err := http.Get(fmt.Sprintf("https://en.wikipedia.org/api/rest_v1/page/summary/%s", wikiTitle))
 		if err != nil {
 			log.Fatal(err)
